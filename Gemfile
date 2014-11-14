465f0cfdbaca2216cf9581ee09bf6829c5eb5472
Carrie <flammia.carrie@gmail.com>
2014-11-14 00:19:32 +0000

    Converted html to haml and added markdown syntax in posts

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'faker'
 gem 'devise'
 gem 'figaro', '1.0'
 gem 'pundit'
+gem 'redcarpet'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,20 @@
 module ApplicationHelper
-	def my_name
-		my_name = Carrie
+  def my_name
+	  my_name = Carrie
 	end
 
 	def form_group_tag(errors, &block)
-		if errors.any?
-			content_tag :div, capture(&block), class: 'form-group has-error'
-		else
-			content_tag :div, capture(&block), class: 'form-group'
+	  if errors.any?
+	  content_tag :div, capture(&block), class: 'form-group has-error'
+	  else
+		  content_tag :div, capture(&block), class: 'form-group'
 		end
 	end
+
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render text).html_safe
+  end
 end
