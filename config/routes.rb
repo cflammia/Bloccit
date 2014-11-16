9e20352a4ea238aba79cb3d854b5dbbfa8d863cd
Carrie <flammia.carrie@gmail.com>
2014-11-16 20:21:29 +0000

    Fixed routes nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Bloccit::Application.routes.draw do
-  get "comments/create"
   devise_for :users
   	resources :users, only: [:update]
   resources :topics do
   	resources :posts, except: [:index]
-    resources :comments, only: [:create]
+      resources :comments, only: [:create]
   end
 
   get 'about' => 'welcome#about'
