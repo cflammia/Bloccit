1ab6fb1dd22cac06cebb61ef5a95ec0342b78a99
Carrie <flammia.carrie@gmail.com>
2014-11-20 19:36:16 +0000

    Added voting system to Bloccit

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Bloccit::Application.routes.draw do
   resources :users, only: [:update]
 
   resources :topics do
-  	resources :posts, except: [:index] do
+  	resources :posts, except: [:index]
+  end
+
+  resources :posts, only: [] do
       resources :comments, only: [:create, :destroy]
-    end
+
+      post '/up-vote' => 'votes#up_vote', as: :up_vote
+      post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
 
   get 'about' => 'welcome#about'
